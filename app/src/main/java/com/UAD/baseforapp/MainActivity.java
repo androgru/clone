d6cda80dfcee408c6d93cdb2fdb3d1288cbf3c95
RMX999 <aljithchonedan999@gmail.com>
2020-10-13 17:51:01 +0530

    first commit

---
+++ app/src/main/java/com/UAD/baseforapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.UAD.baseforapp;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+public class MainActivity extends AppCompatActivity
+{  @Override 
+protected void onCreate(Bundle savedInstanceState) {  super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	
+	
+	}
+		
+		
+}
